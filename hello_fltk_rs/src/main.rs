4ef3177ee490fc2257fd558c65f607f0724fe7a7
Coby Tamayo <acobster@gmail.com>
2021-07-05 21:08:15 -0700

    get anyhow, cpal installed, do basic device setup

--- hello_fltk_rs/src/main.rs
+++ hello_fltk_rs/src/main.rs
@@ -1,9 +1,23 @@
+extern crate anyhow;
+
+use cpal::{Data, Sample};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use fltk::{app, prelude::*, window::Window};
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    // Set up an audio Device.
+    let host = cpal::default_host();
+    let device = host.default_output_device()
+        .expect("no output device available.");
+    println!("Default output device chosen: {}", device.name()?);
+
+    //let config = device.default_output_config
+
     let app = app::App::default();
     let mut window = Window::new(100, 100, 400, 300, "Hello from Rust! 🦀");
     window.end();
     window.show();
     app.run().unwrap();
+
+    Ok(())
 }
